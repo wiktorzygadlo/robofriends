39714f4475613bb714529d951d15d82aad67c841
wiktorzygadlo <54442899+wiktorzygadlo@users.noreply.github.com>
2019-10-20 16:53:28 +0100

    Updates

---
+++ precache-manifest.c8e2ab647a2b28fc4c0381e25da799a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f996b1dda50fb669ec3a93e9f334d2",
+    "url": "/robofriendsv2/index.html"
+  },
+  {
+    "revision": "d2d58a1f003a77f58cfd",
+    "url": "/robofriendsv2/static/css/main.ddf55836.chunk.css"
+  },
+  {
+    "revision": "84fe3be2ee2cceeb8c5b",
+    "url": "/robofriendsv2/static/js/2.c49aa75f.chunk.js"
+  },
+  {
+    "revision": "d2d58a1f003a77f58cfd",
+    "url": "/robofriendsv2/static/js/main.48b4a183.chunk.js"
+  },
+  {
+    "revision": "e1b69708255e5734c712",
+    "url": "/robofriendsv2/static/js/runtime-main.b01fe612.js"
+  }
+]);
